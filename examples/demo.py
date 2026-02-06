89d6b3b1d0e774699398df25210e16c84784cd24
Sokolov <227881975+sokolov980@users.noreply.github.com>
2026-02-05 22:17:25 -0500

    Delete examples/demo.py

--- examples/demo.py
+++
@@ -1,7 +0,0 @@
-from src.predictor import SurnamePredictor
-
-predictor = SurnamePredictor()
-
-print(predictor.predict("Kowalski"))
-print(predictor.predict("MacArthur"))
-print(predictor.predict("Fernandez"))
